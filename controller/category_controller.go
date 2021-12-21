d1e0032b2f9a0cd71dc3e5de90f3899a02fe3bb1
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2021-12-21 12:04:17 +0700

    fix pointer

--- controller/category_controller.go
+++ controller/category_controller.go
@@ -7,9 +7,9 @@ import (
 )
 
 type CategoryController interface {
-	Create(rw http.ResponseWriter, r http.Request, p httprouter.Params)
-	Update(rw http.ResponseWriter, r http.Request, p httprouter.Params)
-	Delete(rw http.ResponseWriter, r http.Request, p httprouter.Params)
-	FindById(rw http.ResponseWriter, r http.Request, p httprouter.Params)
-	FindAll(rw http.ResponseWriter, r http.Request, p httprouter.Params)
+	Create(rw http.ResponseWriter, r *http.Request, p httprouter.Params)
+	Update(rw http.ResponseWriter, r *http.Request, p httprouter.Params)
+	Delete(rw http.ResponseWriter, r *http.Request, p httprouter.Params)
+	FindById(rw http.ResponseWriter, r *http.Request, p httprouter.Params)
+	FindAll(rw http.ResponseWriter, r *http.Request, p httprouter.Params)
 }
