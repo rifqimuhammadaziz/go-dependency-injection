adc4387af953b9c935ea898f878179688f2500e1
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2021-12-22 09:25:43 +0700

    fix sql query

--- repository/category_repository_impl.go
+++ repository/category_repository_impl.go
@@ -17,7 +17,7 @@ func NewCategoryRepository() CategoryRepository {
 }
 
 func (repository *CategoryRepositoryImpl) Save(ctx context.Context, tx *sql.Tx, category domain.Category) domain.Category {
-	SQL := "INSERT INTO customer(name) values(?)"
+	SQL := "INSERT INTO category(name) values(?)"
 	result, err := tx.ExecContext(ctx, SQL, category.Name)
 	helper.PanicIfError(err)
 
