b258e407231702bfaae27b07f9bf795593ac14c0
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2021-12-22 16:53:32 +0700

    add error handler request

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/julienschmidt/httprouter"
 	"github.com/rifqimuhammadaziz/go-restful-api/app"
 	"github.com/rifqimuhammadaziz/go-restful-api/controller"
+	"github.com/rifqimuhammadaziz/go-restful-api/exception"
 	"github.com/rifqimuhammadaziz/go-restful-api/helper"
 	"github.com/rifqimuhammadaziz/go-restful-api/repository"
 	"github.com/rifqimuhammadaziz/go-restful-api/service"
@@ -28,6 +29,8 @@ func main() {
 	router.POST("/api/categories", categoryController.Create)
 	router.PUT("/api/categories/:categoryId", categoryController.Update)
 	router.DELETE("/api/categories/:categoryId", categoryController.Delete)
+
+	router.PanicHandler = exception.ErrorHandler
 
 	server := http.Server{
 		Addr:    "localhost:3000",
